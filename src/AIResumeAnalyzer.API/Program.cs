fa3fdff0010e7fec37033a1466fcd90b4c327e88
Shashi <shashi070910@gmail.com>
2026-04-21 10:08:04 +0530

    Added DbContext and migrations

--- src/AIResumeAnalyzer.API/Program.cs
+++ src/AIResumeAnalyzer.API/Program.cs
@@ -1,13 +1,17 @@
+using AIResumeAnalyzer.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
