d6907683a8ef139d4676c8b767801cfb7d4e6d1e
Shashi <shashi070910@gmail.com>
2026-04-22 10:42:52 +0530

    Added HuggingFace API Key and registered HttpClient

--- src/AIResumeAnalyzer.API/Program.cs
+++ src/AIResumeAnalyzer.API/Program.cs
@@ -2,6 +2,7 @@ using AIResumeAnalyzer.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using AIResumeAnalyzer.Application.Features.Analysis.Services;
 using AIResumeAnalyzer.Infrastructure.Repositories;
+using AIResumeAnalyzer.Infrastructure.AI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpClient();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IAnalysisRepository, AnalysisRepository>();
+builder.Services.AddScoped<IAIService, HuggingFaceService>();
 builder.Services.AddScoped<IAnalysisService, AnalysisService>();
 
 var app = builder.Build();
