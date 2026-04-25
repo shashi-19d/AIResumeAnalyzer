a6b7aff44a9f294b30058bdabb855cb0432ec4dd
Shashi <shashi070910@gmail.com>
2026-04-25 11:53:28 +0530

    Added new controller to upload resume

--- src/AIResumeAnalyzer.API/Controllers/AnalysisController.cs
+++ src/AIResumeAnalyzer.API/Controllers/AnalysisController.cs
@@ -1,4 +1,7 @@
 ﻿using AIResumeAnalyzer.Application.Features.Analysis.Services;
+using AIResumeAnalyzer.Infrastructure.Parsing;
+using iText.Kernel.XMP.Impl;
+using iText.StyledXmlParser.Jsoup.Parser;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AIResumeAnalyzer.API.Controllers;
@@ -8,10 +11,12 @@ namespace AIResumeAnalyzer.API.Controllers;
 public class AnalysisController : ControllerBase
 {
     private readonly IAnalysisService _service;
+    private readonly ResumeParserService _parser;
 
-    public AnalysisController(IAnalysisService service)
+    public AnalysisController(IAnalysisService service, ResumeParserService parser)
     {
         _service = service;
+        _parser = parser;
     }
 
     [HttpPost("analyze")]
@@ -19,5 +24,18 @@ public class AnalysisController : ControllerBase
     {
         var result = await _service.AnalyzeAsync(request);
         return Ok(result);
+    }
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadResume([FromForm] UploadResumeDto dto)
+    {
+        if (dto.File == null || dto.File.Length == 0)
+            return BadRequest("File is required");
+
+        using var stream = dto.File.OpenReadStream();
+
+        var text = _parser.ExtractText(stream);
+
+        return Ok(new { content = text });
     }
 }
